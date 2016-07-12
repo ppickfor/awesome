a675b445b5c3c513d321d36a36b5eb2fca1fa3af
Peter Pickford <ppickfor@users.noreply.github.com>
2016-07-11 21:03:15 -0700

    make KP_Enter behave same as Return incase numlock is on

--- rc.lua
+++ rc.lua
@@ -286,6 +286,7 @@ globalkeys = awful.util.table.join(
 
     -- Standard program
     awful.key({ modkey,           }, "Return", function () awful.util.spawn(terminal) end),
+    awful.key({ modkey,           }, "KP_Enter", function () awful.util.spawn(terminal) end),
     awful.key({ modkey, "Control" }, "r", awesome.restart),
     awful.key({ modkey, "Shift"   }, "q", awesome.quit),
 
